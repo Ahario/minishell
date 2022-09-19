5e142f88f93ab9451af865255facc3daa27ef65f
Ahario <81345604+Ahario@users.noreply.github.com>
2022-09-19 20:09:43 +0900

    Add files via upload

---
+++ almost done/libft/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeo <hyeo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 13:27:53 by hyeo              #+#    #+#             */
+/*   Updated: 2021/12/03 14:02:42 by hyeo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*dest;
+
+	i = 0;
+	dest = (unsigned char *)b;
+	while (i != len)
+	{
+		dest[i] = (unsigned char)c;
+		i++;
+	}
+	return (dest);
+}
---
+++ almost done/minishell.h
@@ -0,0 +1,45 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <termios.h>
+# include <unistd.h>
+#include <sys/signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+
+typedef struct s_arg{
+	char	*ac;
+	int		type;
+	int		fd_in;
+	int		fd_out;
+	struct s_arg	*next;
+}	t_arg;
+
+typedef struct s_data{
+	t_arg	*cmd;
+	int		argc;
+	char	**argv;
+	char	**envp;
+}	t_data;
+
+
+enum	e_pars{
+    NORM, SPCE, SINQ, DOUQ, BSLA, DOLR,
+    PIPE, DPIP, SEMC, DSEM,
+    RDRT, DRGT
+};
+
+//t_arg	*set_cmd(char *ch);
+void	before_init(void);
+void	ft_signal(void);
+void	ft_exit(void);
+
+void replace_ds_parse(t_data *data);
+char *replace_dollar_sign(char *str, t_data *data);
+void replace_parse(t_data *data);
+int get_temp_size(char *str);
+
+#endif
